f83b61c60d2e9737da56eadc04579689aac89a3f
Liteno <lukaszcholewka@outlook.com>
2024-04-03 01:19:17 +0200

    Safety commit before OS change

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
+require('dotenv').config();
+const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const number = 2 * 2 + 2;
+app.use((0, express_session_1.default)({
+    secret: "password",
+    saveUninitialized: true,
+    resave: false,
+}));
+app.get('/', (req, res) => {
+    console.log("request handled !");
+    res.json({ data: number });
+    res.end();
+});
 app.listen(3000, () => {
     console.log("server is listening on port : 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+require('dotenv').config();
 import express from "express";
+import session from "express-session";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+const number = 2 * 2 + 2;
 
-app.listen(3000, ()=>{
+app.use(session({
+  secret: "password", // change to env file later
+  saveUninitialized: true,
+  resave: false,
+}));
+
+app.get('/', (req, res) => {
+  console.log("request handled !");
+  res.json({ data : number });
+  res.end();
+});
+
+app.listen(3000, () => {
   console.log("server is listening on port : 3000");
-})
+}); 
