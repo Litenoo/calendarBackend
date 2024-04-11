9774e23b4b1c8b76ae8c7655e3df652a2a255a13
Litenoo <lukaszcholewka@outlook.com>
2024-04-11 23:29:07 +0200

    Patch added .env file to every file

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -8,7 +8,7 @@ const pool = mariaDB.createPool({ //WARNING connected database gets empty every 
   host: "127.0.0.1",
   port: 3306,
   user: 'root',
-  password: 'Pikachu531',
+  password: process.env.DATABASE_PASSWORD,
   database: 'testCalendarApp',
   connectionLimit: 10,
 });
